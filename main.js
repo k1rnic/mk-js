e74669728a1fedf466d647821a0b5523ce95cd78
k1rnic <knikolskiy.io@gmail.com>
2021-04-11 03:03:50 +0300

    feat(homework-3): done
    - Rewrited logic with classes
    - Added random punch feat

--- main.js
+++ main.js
@@ -1,68 +1,105 @@
-function createCharacter(name, weapon = ['заточка']) {
-  return {
-    name,
-    hp: 100,
-    weapon,
-    attack: function () {
-      console.log(`${name} Fight...`);
-    },
-  };
-}
+const CHARACTERS = {
+  policewoman: {
+    weapons: ['handcuffs'],
+  },
+  stepsister: {
+    weapons: ['cross'],
+  },
+};
 
-function htmlCreate(element, ...classNames) {
+function createElement(element, ...classNames) {
   const $htmlElement = document.createElement(element);
   $htmlElement.classList.add(...classNames);
 
   return $htmlElement;
 }
 
-function createPlayer(player, character) {
-  function createLifePanel(character) {
-    const $container = htmlCreate('div', 'progressbar');
-
-    const $lifeIndicator = htmlCreate('div', 'life', `life-${character.hp}`);
-    const $characterName = htmlCreate('div', 'name');
-
-    $characterName.innerText = character.name;
-
-    $container.append($lifeIndicator, $characterName);
-
-    return $container;
+class Player {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.hp = 100;
+    this.characteristic = CHARACTERS[this.name];
+    this.createElement();
   }
 
-  function createCharacter(character) {
-    console.log(character);
+  createElement = () => {
+    this.$element = createElement('div', `player${this.id}`);
 
-    const $container = htmlCreate('div', 'character');
-    const $characterImg = htmlCreate('img', 'character-img');
+    this.createLifePanel();
+    this.createCharacterPlaceholder();
+  };
 
-    $characterImg.setAttribute(
-      'src',
-      `./assets/fighters/${character.name}.gif`,
-    );
+  createLifePanel = () => {
+    this.$lifePanel = createElement('div', 'progressbar');
 
-    $container.appendChild($characterImg);
+    this.$lifeIndicator = createElement('div', 'life');
+    this.$lifeIndicator.style.width = `${this.hp}%`;
 
-    return $container;
+    this.$characterName = createElement('div', 'name');
+    this.$characterName.innerText = this.name;
+
+    this.$lifePanel.append(this.$lifeIndicator, this.$characterName);
+    this.$element.appendChild(this.$lifePanel);
+  };
+
+  createCharacterPlaceholder = () => {
+    this.$characterPlaceholder = createElement('div', 'character');
+    const $characterImg = createElement('img', 'character-img');
+
+    $characterImg.setAttribute('src', `./assets/fighters/${this.name}.gif`);
+
+    this.$characterPlaceholder.appendChild($characterImg);
+    this.$element.appendChild(this.$characterPlaceholder);
+  };
+
+  damage = (damage) => {
+    this.hp = Math.max(0, this.hp - damage);
+    this.$lifeIndicator.style.width = `${this.hp}%`;
+  };
+
+  punch = (enemy) => {
+    const damage = Math.ceil(Math.random() * 20);
+    enemy.damage(damage);
+  };
+}
+
+class Arena {
+  constructor(players) {
+    this.$element = document.querySelector('.arenas');
+    this.players = players;
+    this.players.forEach((player) => {
+      this.$element.appendChild(player.$element);
+    });
   }
 
-  const $container = htmlCreate('div', player);
-
-  const lifePanel = createLifePanel(character);
-  $container.appendChild(lifePanel);
-  $container.appendChild(createCharacter(character));
-
-  return $container;
+  congratulate(winnerName) {
+    const $congratulationTitle = createElement('div', 'winnerTitle');
+    $congratulationTitle.innerText = `${winnerName} wins`;
+    this.$element.appendChild($congratulationTitle);
+  }
 }
 
-function addToArena(player) {
-  const $arena = document.querySelector('.arenas');
+class Game {
+  constructor(players) {
+    this.arena = new Arena(players);
+    this.$punchTrigger = document.querySelector('.punch-trigger');
+    this.$punchTrigger.addEventListener('click', this.luckyPunch);
+  }
 
-  $arena.appendChild(player);
+  randomize = () => {
+    return this.arena.players.sort(() => Math.random() - 0.5);
+  };
+
+  luckyPunch = () => {
+    const [punisher, enemy] = this.randomize();
+    punisher.punch(enemy);
+
+    if (enemy.hp === 0) {
+      this.$punchTrigger.disabled = true;
+      this.arena.congratulate(punisher.name);
+    }
+  };
 }
 
-const playerOne = createPlayer('player1', createCharacter('policewoman'));
-const playerTwo = createPlayer('player2', createCharacter('sister'));
-
-addToArena(playerOne);
-addToArena(playerTwo);
+const game = new Game([new Player(1, 'policewoman'), new Player(2, 'sister')]);
